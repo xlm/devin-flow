317b9a849029c05e7eedb8bef9bbdfb0bed4470c
xieliming <xieliming@gmail.com>
2026-09-20 09:15:52 +0000

    Revert "feat(frontend): use smoothstep edges"
    This reverts commit 7adc88ff0ee69883a9136f2f9e6488dc0e342c39.

--- frontend/src/components/FlowCanvas.vue
+++ frontend/src/components/FlowCanvas.vue
@@ -816,8 +816,6 @@ onUnmounted(() => {
         :nodes="nodes"
         :edges="edges"
         :node-types="nodeTypes"
-        :default-edge-options="{ type: 'smoothstep' }"
-        :apply-default="false"
         fit-view-on-init
         :is-valid-connection="validConnection"
       >
